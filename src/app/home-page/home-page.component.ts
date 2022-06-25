a7bd73fda7f7ae0fe65ff8f11fe6f3e558bc7dbc
Mahdyar <m4hdyar@gmail.com>
2022-06-25 12:40:55 +0200

    Add category to the table

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -6,28 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-  columnsToDisplay = ['title', 'description', 'start_date', 'end_date'];
+  columnsToDisplay = ['title','category','description', 'start_date', 'end_date'];
+
   events = [
     {
       "title": "First event",
+      "category" : "education",
       "description": "First event",
       "startDate": "2017-10-10",
       "endDate": "2017-10-12"
     },
     {
       "title": "Second",
+      "category" : "education",
       "description": "Second event",
       "startDate": "2017-10-10",
       "endDate": "2017-10-12"
     },
     {
       "title": "Third",
+      "category" : "Party",
       "description": "Third event",
       "startDate": "2017-10-10",
       "endDate": "2017-10-12"
     },
     {
       "title": "Fourth",
+      "category" : "Sport",
       "description": "Fourth event",
       "startDate": "2017-10-10",
       "endDate": "2017-10-12"
