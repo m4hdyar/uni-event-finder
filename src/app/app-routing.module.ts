457e813ed68122e36599e860d66e7f57d1a78226
nahid4095 <nahid.nezhad65@gmail.com>
2022-06-27 11:15:04 +0200

    Add register button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,12 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path: 'admin-panel', component: AdminPanelComponent},
+  {path: 'register', component: RegisterComponent},
+  
   { path: 'login', component:LoginComponent, pathMatch: 'full' },
   { path: '**', component:HomePageComponent, pathMatch: 'full' }
 ];
