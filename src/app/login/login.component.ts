8356548c0da0052c11420a6ff36a291daf420cc3
Mahdyar <m4hdyar@gmail.com>
2022-07-04 02:10:03 +0200

    Add JWT Login functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { NgModule } from  '@angular/core';
-import {MatToolbarModule} from  '@angular/material/toolbar';
-import {MatButtonModule } from '@angular/material/button';
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,11 +13,31 @@ import {MatButtonModule } from '@angular/material/button';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router) {
+
+    this.form = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
   }
-login(){
-  
-}
+  login() {
+    const val = this.form.value;
+
+    if (val.email && val.password) {
+      this.authService.login(val.email, val.password)
+        .subscribe(
+          () => {
+            console.log("User is logged in");
+            this.router.navigateByUrl('/');
+          }
+        );
+    }
+  }
 }
